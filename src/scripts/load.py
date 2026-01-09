36c6a05400ef9d195f0dc5196eb49d1d780272f4
matiasfeliu92 <matumazparrote@gmail.com>
2026-01-09 12:00:08 -0300

    update connections strings in Load class, for use in docker container or windows environemnt

--- src/scripts/load.py
+++ src/scripts/load.py
@@ -1,5 +1,6 @@
 import logging
 import pandas as pd
+import platform
 
 from src.config.settings import Settings
 from src.config.db import ManageDB
@@ -12,16 +13,15 @@ logging.basicConfig(
 )
 
 class Load:
-    def __init__(self, host=None):
-        logging.info(f"------------HOST: {host} IN LOAD------------")
+    def __init__(self):
         self.settings = Settings()
         self.manage_db = ManageDB()
         self.utils = Utils()
-        self.conn_string_default_DB = self.settings.POSTGRES_CONNECTION_STRING_DEFAULT_DB if host is None else self.settings.POSTGRES_CONNECTION_STRING_DOCKER_DEFAULT_DB
-        self.conn_string_for_cursor_default_DB = self.settings.POSTGRES_CURSOR_CONNECTION_STRING_DEFAULT_DB if host is None else self.settings.POSTGRES_CURSOR_CONNECTION_STRING_DOCKER_DEFAULT_DB
+        self.conn_string_default_DB = self.settings.POSTGRES_CONNECTION_STRING_DEFAULT_DB if platform.system() == "Windows" else self.settings.POSTGRES_CONNECTION_STRING_DOCKER_DEFAULT_DB
+        self.conn_string_for_cursor_default_DB = self.settings.POSTGRES_CURSOR_CONNECTION_STRING_DEFAULT_DB if platform.system() == "Windows" else self.settings.POSTGRES_CURSOR_CONNECTION_STRING_DOCKER_DEFAULT_DB
         self.engine = None
-        self.conn_string_new_DB = self.settings.POSTGRES_CONNECTION_STRING_NEW_DB if host is None else self.settings.POSTGRES_CONNECTION_STRING_DOCKER_NEW_DB
-        self.conn_string_for_cursor_new_DB = self.settings.POSTGRES_CURSOR_CONNECTION_STRING_NEW_DB if host is None else self.settings.POSTGRES_CURSOR_CONNECTION_STRING_DOCKER_NEW_DB
+        self.conn_string_new_DB = self.settings.POSTGRES_CONNECTION_STRING_NEW_DB if platform.system() == "Windows" else self.settings.POSTGRES_CONNECTION_STRING_DOCKER_NEW_DB
+        self.conn_string_for_cursor_new_DB = self.settings.POSTGRES_CURSOR_CONNECTION_STRING_NEW_DB if platform.system() == "Windows" else self.settings.POSTGRES_CURSOR_CONNECTION_STRING_DOCKER_NEW_DB
 
     def load_data_in_DB(self, __df__: pd.DataFrame, __league__, __season__):
         logging.info(f"-------CONNECT USING {self.conn_string_for_cursor_default_DB}-------")

74c55ec486cfd92a4f6cb5798bbe300fa37d43fc
matiasfeliu92 <matumazparrote@gmail.com>
2026-01-12 14:12:15 +0000

    add settings module for scrapper config

---
+++ src/config/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    BASE_URL = "https://www.football-data.co.uk"
+    ENDPOINTS = [
+        "/englandm.php",
+        "/italym.php",
+        "/spainm.php",
+        # "/brazil.php",
+        # "/argentina.php",
+        # "/usa.php"
+    ]
